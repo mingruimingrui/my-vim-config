2d8bfcc5e7d85cef371a200293b24327a8ad4f51
Wang Ming Rui <mingrui.wang@C02CJTUYMD6M.local>
2020-05-15 10:30:48 +0800

    Add rsync helper

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -2,6 +2,8 @@
 unsetopt share_history
 unsetopt BEEP
 
+######## Brew
+
 if [ $(command -v brew) ]; then
 	function brew-upgrade-clean() {
 		brew update
@@ -14,16 +16,25 @@ if [ $(command -v brew) ]; then
 	}
 fi
 
+######## Python
+
+CONDA_INIT_PATH=$HOME/miniconda3/etc/profile.d/conda.sh
+if [ -f $CONDA_INIT_PATH ]; then
+	. $CONDA_INIT_PATH
+	function conda-upgrade-base() {
+		conda update -n base -c defaults conda
+	}
+fi
+
+######## Neovim
+
 if [ $(command -v nvim) ]; then
 	alias vim=nvim
-
 	function install-nvim-py-pkgs() {
 		pip install --no-cache-dir neovim jedi
 	}
 fi
 
-CONDA_INIT_PATH=$HOME/miniconda3/etc/profile.d/conda.sh
-if [ -f $CONDA_INIT_PATH ]; then
-	. $CONDA_INIT_PATH
-fi
+######## rsync
 
+alias rsync="rsync --cvs-ignore --max-size=10m"
